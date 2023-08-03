b6429cc305d43fbc3fa590ea32e5714e452b18bb
95Donguk <midgard95@nate.com>
2023-08-03 19:22:41 +0900

    refactor(Problem6): 중복 닉네임 이메일 구하는 로직 스트림 연산으로 변환

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,24 +1,20 @@
 package onboarding;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class Problem6 {
+    private static final Map<String, String> dividedNicknameRepo = new HashMap<>();
 
     public static void main(String[] args) {
         List<List<String>> forms = List.of(
-            List.of("jm@email.com", "제이엠"),
-            List.of("jason@email.com", "제이슨"),
-            List.of("woniee@email.com", "워니"),
-            List.of("mj@email.com", "엠제이"),
-            List.of("nowm@email.com", "이제엠")
+                List.of("jm@email.com", "제이엠"),
+                List.of("jason@email.com", "제이슨"),
+                List.of("woniee@email.com", "워니"),
+                List.of("mj@email.com", "엠제이"),
+                List.of("nowm@email.com", "이제엠")
         );
 
         System.out.printf("%-30s|%-30s|", "forms", "result");
@@ -34,42 +30,33 @@ public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         FormsValidator.validate(forms);
-        answer = findEmailsWithLimitedNicknames(forms);
+        answer = forms.stream()
+                .map(Problem6::findEmailsWithLimitedNicknames)
+                .flatMap(Collection::stream)
+                .distinct()
+                .sorted()
+                .collect(Collectors.toList());
         return answer;
     }
 
-    private static List<String> findEmailsWithLimitedNicknames(List<List<String>> forms) {
-        Map<String, String> charDuplicateCheckRepo = new HashMap<>();
-        Set<String> resultEmails = new HashSet<>();
-        for (List<String> form : forms) {
-            String email = form.get(0);
-            String nickname = form.get(1);
-            findNicknamesWithDuplicateCharacters(charDuplicateCheckRepo, resultEmails, email,
-                nickname);
-        }
-        return resultEmails.stream().sorted().collect(Collectors.toList());
-    }
+    private static Set<String> findEmailsWithLimitedNicknames(List<String> form) {
+        Set<String> limitedNicknameEmails = new HashSet<>();
 
-    private static void findNicknamesWithDuplicateCharacters(
-        Map<String, String> charDuplicateCheckRepo,
-        Set<String> resultEmails, String email, String nickname) {
+        String email = form.get(0);
+        String nickname = form.get(1);
 
-        for (int i = 0; i < nickname.length() - 1; i++) {
-            String charScanner = nickname.substring(i, i + 2);
-            if (charDuplicateCheckRepo.containsKey(charScanner)) {
-                String charScannerEmail = charDuplicateCheckRepo.get(charScanner);
-                getResultEmail(resultEmails, email, charScannerEmail);
+        for (int charIndex = 0; charIndex < nickname.length() - 1; charIndex++) {
+            String dividedNickname = nickname.substring(charIndex, charIndex + 2);
+            String limitedNicknameEmail = dividedNicknameRepo.putIfAbsent(dividedNickname, email);
+            if (!Objects.isNull(limitedNicknameEmail)) {
+                limitedNicknameEmails.add(limitedNicknameEmail);
             }
-            charDuplicateCheckRepo.put(charScanner, email);
         }
-    }
 
-    private static void getResultEmail(Set<String> resultEmails, String email,
-                                       String charScannerEmail) {
-        if (!Objects.equals(charScannerEmail, email)) {
-            resultEmails.add(charScannerEmail);
-            resultEmails.add(email);
+        if (!limitedNicknameEmails.isEmpty()) {
+            limitedNicknameEmails.add(email);
         }
+        return limitedNicknameEmails;
     }
 
     private static class FormsValidator {
@@ -78,7 +65,7 @@ public class Problem6 {
         public static void validate(List<List<String>> forms) {
             if (checkNumberOfCrews(forms)) {
                 throw new IllegalArgumentException(
-                    "A crew consists of at least 1 and no more than 10,000 people.");
+                        "A crew consists of at least 1 and no more than 10,000 people.");
             }
             for (List<String> form : forms) {
                 EmailValidator.validate(form.get(0));
@@ -101,12 +88,12 @@ public class Problem6 {
 
                 if (checkLengthOfEmail(email)) {
                     throw new IllegalArgumentException(
-                        "Emails should be at least 11 and less than 20 characters in length.");
+                            "Emails should be at least 11 and less than 20 characters in length.");
                 }
 
                 if (!isValidEmailDomain(email)) {
                     throw new IllegalArgumentException(
-                        "Emails can only be submitted with the domain \"email.com\".");
+                            "Emails can only be submitted with the domain \"email.com\".");
                 }
             }
 
@@ -139,7 +126,7 @@ public class Problem6 {
 
                 if (checkNicknameLength(nickname)) {
                     throw new IllegalArgumentException(
-                        "Nicknames should be at least 11 and less than 20 characters in length.");
+                            "Nicknames should be at least 11 and less than 20 characters in length.");
                 }
             }
 
